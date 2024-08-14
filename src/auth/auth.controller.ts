718b3897e738d77e889f11d77f0d4d046fb2ad63
caesarKom <cezarykomorowski@gmail.com>
2024-08-14 17:46:28 +0200

    add apiKey

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UseGuards
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { UserObj } from 'src/decorators/user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
+
   @Post('login')
   login(@Body() { email, password }: LoginDto) {
     return this.authService.login(email, password);
+  }
+
+  @Post('register')
+  @HttpCode(201)
+  register(@Body() createUserDto: CreateUserDto) {
+    return this.authService.register(createUserDto);
+  }
+
+  @Get('api-key')
+  @UseGuards(JwtAuthGuard)
+  getApiKey(@UserObj() user: User) {
+    return this.authService.getApiKey(user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,51 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { DbService } from 'src/db/db.service';
 import { AuthEnity } from './entity/auth.entity';
-import * as bcrypt from "bcrypt";
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-    constructor(private db: DbService, private jwtService: JwtService) {}
-    
-    async login(email:string,password:string): Promise<AuthEnity> {
-        const user = await this.db.user.findUnique({where:{email}});
-        if (!user) {
-            throw new NotFoundException(`No user found for email: ${email}`)
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password)
-        if (!isPasswordValid) {
-            throw new UnauthorizedException('Invalid credentials');
-          }
-          return {
-            at: this.jwtService.sign({ userId: user.id })
-          }
+  constructor(
+    private db: DbService,
+    private jwtService: JwtService,
+    private userService: UsersService
+  ) {}
+
+  async login(email: string, password: string): Promise<AuthEnity> {
+    const user = await this.db.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new NotFoundException(`No user found for email: ${email}`);
     }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return {
+      at: this.jwtService.sign({ userId: user.id })
+    };
+  }
+
+  async register(createUserDto: CreateUserDto) {
+    await this.userService.create(createUserDto);
+
+    return { message: 'User registered successfully' };
+  }
+
+  async getApiKey(user: User) {
+    const key = await this.db.user.findUnique({
+      where: { apiKey: user.apiKey }
+    });
+    if (!key) {
+      throw new NotFoundException(`Not Found Key!`);
+    }
+    return { apiKey: key };
+  }
 }
