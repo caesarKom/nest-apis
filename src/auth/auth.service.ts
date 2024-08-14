8d3ca7b0f394471a6a450874403177b59bf503e9
caesarKom <cezarykomorowski@gmail.com>
2024-08-14 16:09:22 +0200

    add decorator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
         }
         const isPasswordValid = await bcrypt.compare(password, user.password)
         if (!isPasswordValid) {
-            throw new UnauthorizedException('Invalid credential');
+            throw new UnauthorizedException('Invalid credentials');
           }
           return {
             at: this.jwtService.sign({ userId: user.id })
